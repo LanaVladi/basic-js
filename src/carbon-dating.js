66de315141a4238b3ad586f1d5dad2ac000da839
Svetlana <lanavladi1605gmail.com>
2022-04-30 18:09:50 +0500

    feat: add solutions carbon-dating & count-cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,28 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+ function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  // let sampleActivity = '3';
+  const MODERN_ACTIVITY = 15;
+  const HALF_LIFE_PERIOD = 5730;
+  // const ln2 = 0.693;
+  //  const constantReaction = ln2 / HALF_LIFE_PERIOD;
+  const constantReaction = (Math.log(2)) / HALF_LIFE_PERIOD;
+  // Math.log(2);
+  // console.log('Math.log(2) :', Math.log(2));
+  // console.log('sampleActivity === false :', sampleActivity === false);
+  // console.log('sampleActivity === null :', sampleActivity === null);
+  // console.log('sampleActivity === undefined :', sampleActivity === undefined);
+  // console.log('typeof (sampleActivity) !== \'string\' :', typeof (sampleActivity) !== 'string');
+  // console.log('sampleActivity < 1 :', sampleActivity < 1);
+  // console.log('sampleActivity > 15 :', sampleActivity > 15);
+  if (typeof(sampleActivity) !== 'string' || isNaN(sampleActivity) || sampleActivity === undefined || sampleActivity === null || sampleActivity === false || sampleActivity > 15 || sampleActivity < 1) return false;
+  // console.log('false :', false);
+  let dateSample = Math.ceil(Math.abs((Math.log(sampleActivity / MODERN_ACTIVITY)) / constantReaction));
+  return dateSample;
+  // console.log('dateSample :', dateSample);
 }
 
 module.exports = {
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+ function countCats(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  // let matrix =  [
+  //   ['##', 'dd', '00'],
+  //   ['^^', '..', 'ss'],
+  //   ['AA', 'dd', 'Oo'],
+  // ];
+  let result;
+  let countResults = [];
+  if (matrix.length === 0) {
+      // console.log('matrix.length :', matrix.length);
+      result = 0;
+  } else {
+      for (let i = 0; i < matrix.length; i++) {
+          //  console.log('matrix :', matrix);
+          for (let j = 0; j < matrix[i].length; j++) {
+              if (matrix[i][j] !== '^^') {
+                  // console.log('matrix[i][j] :', matrix[i][j]);
+                  result = 0;
+              } else {
+                  //  if (matrix[i][j] === '^^') {
+                  // console.log('matrix[i][j] :', matrix[i][j]);
+                  countResults.push(matrix[i][j]);
+                  //  countResults.length; 
+                  //  console.log('countResults:', countResults);
+                  // console.log('result :', result);
+              }
+              // return result;
+          }
+      }
+  }
+  return countResults.length;
 }
+
 
 module.exports = {
   countCats

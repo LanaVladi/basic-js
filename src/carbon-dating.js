c3a6e274ac94495075e39eeb3ec84d8601d22933
Svetlana <lanavladi1605gmail.com>
2022-10-18 03:59:20 +0500

    refactor: solution carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -6,18 +6,18 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
- function dateSample(sampleActivity) {
+function dateSample(sampleActivity) {
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
   // let sampleActivity = '3';
@@ -25,7 +25,7 @@ const HALF_LIFE_PERIOD = 5730;
   const HALF_LIFE_PERIOD = 5730;
   // const ln2 = 0.693;
   //  const constantReaction = ln2 / HALF_LIFE_PERIOD;
-  const constantReaction = (Math.log(2)) / HALF_LIFE_PERIOD;
+  const constantReaction = Math.log(2) / HALF_LIFE_PERIOD;
   // Math.log(2);
   // console.log('Math.log(2) :', Math.log(2));
   // console.log('sampleActivity === false :', sampleActivity === false);
@@ -34,13 +34,24 @@ const HALF_LIFE_PERIOD = 5730;
   // console.log('typeof (sampleActivity) !== \'string\' :', typeof (sampleActivity) !== 'string');
   // console.log('sampleActivity < 1 :', sampleActivity < 1);
   // console.log('sampleActivity > 15 :', sampleActivity > 15);
-  if (typeof(sampleActivity) !== 'string' || isNaN(sampleActivity) || sampleActivity === undefined || sampleActivity === null || sampleActivity === false || sampleActivity > 15 || sampleActivity < 1) return false;
+  if (
+    typeof sampleActivity !== "string" ||
+    isNaN(sampleActivity) ||
+    sampleActivity === undefined ||
+    sampleActivity === null ||
+    sampleActivity === false ||
+    sampleActivity > 15 ||
+    sampleActivity < 1
+  )
+    return false;
   // console.log('false :', false);
-  let dateSample = Math.ceil(Math.abs((Math.log(sampleActivity / MODERN_ACTIVITY)) / constantReaction));
+  let dateSample = Math.ceil(
+    Math.abs(Math.log(sampleActivity / MODERN_ACTIVITY) / constantReaction)
+  );
   return dateSample;
   // console.log('dateSample :', dateSample);
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
